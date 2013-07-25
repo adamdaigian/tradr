48a779fd6b62e4b58cb0f48d5efc5f393bda0565
Baylee Feore <baylee.feore@gmail.com>
2013-07-24 21:11:48 -0700

    Started again without devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradr::Application.config.secret_token = '3d7e10d66283d0bd720600d4592be4b4880dc41978b4ac9714df12bf7cf23729710432fe7afe6a9b53529b3ce37317e713660814398bcfe1d361426bfcd2e4f9'
